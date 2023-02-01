4320624ff4a6255ff83286110cad01933e06b01d
osmanesad <byesad@gmail.com>
2023-02-01 20:36:22 +0300

    Fetch Database
    Firebase veritabanına bağlandık ve verilerimizi çektik. Not: Firebase "Rules" ayarlarında veritabanına erişim kısımını şu şekilde değiştirin "allow read, write: if request.auth !=null;"

--- makeSomeCoffee/View/Home.swift
+++ makeSomeCoffee/View/Home.swift
@@ -6,10 +6,52 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestoreSwift
 
 struct Home: View {
+    @State private var quizInfo: Info?
+    @AppStorage("log_status") private var logStatus: Bool = false
+    @State private var questions: [Question] = []
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        if let info = quizInfo {
+            Text(info.title)
+        } else {
+            VStack(spacing: 4){
+                ProgressView()
+                Text("Lütfen Bekleyin...")
+                    .font(.caption2)
+                    .foregroundColor(.gray)
+            }
+            .task {
+                do {
+                   try await fetchData()
+                } catch {
+                    print("Hata mesajı: \(error.localizedDescription)")
+                }
+            }
+        }
+        
+    }
+    // Hatırlatma: Firebase veritabanı "Rules" ayarlarında veritabanına erişim kısımını şu şekilde değiştirin "allow read, write: if request.auth !=null;"
+    func fetchData()async throws{
+        try await loginUserAnonymous()
+        let info = try await Firestore.firestore().collection("Quiz").document("Info").getDocument().data(as: Info.self)
+        let questions = try await Firestore.firestore().collection("Quiz").document("Info").collection("Questions").getDocuments().documents.compactMap{
+            try $0.data(as: Question.self)
+        }
+        
+        await MainActor.run(body: {
+            self.quizInfo = info
+            self.questions = questions
+        })
+    }
+    
+    func loginUserAnonymous()async throws{
+        if !logStatus {
+            try await Auth.auth().signInAnonymously()
+        }
     }
 }
 
--- makeSomeCoffee/makeSomeCoffeeApp.swift
+++ makeSomeCoffee/makeSomeCoffeeApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct makeSomeCoffeeApp: App {
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
